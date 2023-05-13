d2f31bd55a1afe1c345715022a392a2ec18f4a4a
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-05-13 16:06:35 +0600

    v0.0.5

---
+++ lib/widgets.dart
@@ -0,0 +1,24 @@
+library widgets;
+
+import 'dart:math' show min, max;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+part 'views/expendable_text_view.dart';
+
+part 'views/expensive_scroll_view.dart';
+
+part 'views/flexible_view.dart';
+
+part 'views/notifier_view.dart';
+
+part 'views/stack_layout.dart';
+
+part 'views/text_view.dart';
+
+part 'views/view.dart';
+
+part 'views/view_pager.dart';
+
+part 'views/widget_wrapper.dart';
