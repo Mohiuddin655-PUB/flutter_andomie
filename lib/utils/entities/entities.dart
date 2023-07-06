4184db53e7902f3b8194c93184f20381a524369f
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-07-06 20:51:18 +0600

    v0.4.6

--- lib/utils/entities/entities.dart
+++ lib/utils/entities/entities.dart
@@ -3,15 +3,9 @@ library entities;
 import 'package:intl/intl.dart';
 
 part 'entity.dart';
-
 part 'follow.dart';
-
 part 'like.dart';
-
 part 'report.dart';
-
 part 'search.dart';
-
 part 'star.dart';
-
 part 'viewer.dart';
--- lib/utils/extensions/extensions.dart
+++ lib/utils/extensions/extensions.dart
@@ -3,7 +3,5 @@ library extensions;
 import 'package:flutter/material.dart';
 
 part 'object.dart';
-
 part 'scroll_controller.dart';
-
 part 'string.dart';
