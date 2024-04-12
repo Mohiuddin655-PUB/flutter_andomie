34c6e4101d8a40624e0c00311c5d04fb6ddcf706
Md. Mohi-Uddin <mohiuddin655.developer@gmail.com>
2024-04-12 16:57:40 +0600

    ## 0.5.16
    * Library version updates

--- lib/utils/enums/enums.dart
+++ lib/utils/enums/enums.dart
@@ -1,9 +1,6 @@
 library enums;
 
 part 'action_type.dart';
-
 part 'byte_quality.dart';
-
 part 'data_type.dart';
-
 part 'search_type.dart';
--- lib/utils/extensions/extensions.dart
+++ lib/utils/extensions/extensions.dart
@@ -6,23 +6,13 @@ import 'package:flutter_andomie/core.dart';
 import '../entities/entities.dart';
 
 part 'boolean.dart';
-
 part 'double.dart';
-
 part 'duration.dart';
-
 part 'entity_list.dart';
-
 part 'int.dart';
-
 part 'list.dart';
-
 part 'map.dart';
-
 part 'num.dart';
-
 part 'object.dart';
-
 part 'scroll_controller.dart';
-
 part 'string.dart';
